178be5c37b8f0fdbbfdeba6f2b572572362bba0b
haoran <1759953767@qq.com>
2024-04-05 16:37:26 +0800

    锁

--- webs/until/mutex.h
+++ webs/until/mutex.h
@@ -3,6 +3,7 @@
 
 #include <semaphore.h>
 #include <stdint.h>
+#include <pthread.h>
 #include <mutex>
 
 #include "Noncopyable.h"
@@ -29,28 +30,144 @@ namespace webs
     struct ScopedLockImpl
     {
     public:
+        ScopedLockImpl(T &mutex) : m_mutex(mutex)
+        {
+            m_mutex.lock();
+            m_locked = true;
+        };
+        ~ScopedLockImpl()
+        {
+            unlock();
+        };
         /* 加锁 */
-
+        void lock()
+        {
+            if (!m_locked)
+            {
+                m_mutex.lock();
+                m_locked = true;
+            }
+        };
         /* 解锁 */
+        void unlock()
+        {
+            if (m_locked)
+            {
+                m_mutex.unlock();
+                m_locked = false;
+            }
+        }
+
     private:
-        T& 
+        // mutex：封装互斥量
+        T &m_mutex;
+        //
+        bool m_locked;
     };
 
     /* 局部读锁模板实现 */
     template <class T>
     struct ReadScopedLockImpl
     {
+    public:
+        ReadScopedLockImpl(T &mutex) : m_mutex(mutex)
+        {
+            m_mutex.rdlock();
+            m_locked = true;
+        }
+        ~ReadScopedLockImpl()
+        {
+            unlock();
+        }
+        /* 上读锁 */
+        void lock()
+        {
+            if (!m_locked)
+            {
+                m_mutex.rdlock();
+                m_locked = true;
+            }
+        }
+        /* 释放读锁 */
+        void unlock()
+        {
+            if (m_locked)
+            {
+                m_mutex.unlock();
+                m_locked = false;
+            }
+        }
+
+    private:
+        // mutex
+        T &m_mutex;
+        // 是否已上锁
+        bool m_locked;
     };
 
     /* 局部写锁模板实现 */
     template <class T>
     struct WriteScopedLockImpl
     {
+    public:
+        WriteScopedLockImpl(T &mutex) : m_mutex(mutex)
+        {
+            m_mutex.wrlock();
+            m_locked = true;
+        };
+        ~WriteScopedLockImpl()
+        {
+            unlock();
+        };
+        /* 上写锁 */
+        void lock()
+        {
+            if (!m_locked)
+            {
+                m_mutex.wrlock();
+                m_locked = true;
+            }
+        }
+        /* 解写锁 */
+        void unlock()
+        {
+            if (m_locked)
+            {
+                m_mutex.unlock();
+                m_locked = false;
+            }
+        }
+
+    private:
+        // 写锁
+        T &m_mutex;
+        // 是否已上锁
+        bool m_locked;
     };
 
     /* 互斥锁 */
     class Mutex : Noncopyable
     {
+        public:
+            /// 局部锁
+            typedef ScopedLockImpl<Mutex> Lock;
+            Mutex(){
+                pthread_mutex_init(&m_mutex, nullptr);
+            };
+            ~Mutex(){
+                pthread_mutex_destroy(&m_mutex);
+            };
+            /* 加锁 */
+            void lock(){
+                pthread_mutex_lock(&m_mutex);
+            };
+            /* 解锁 */
+            void unlock(){
+                pthread_mutex_unlock(&m_mutex);
+            };
+
+        private:
+            pthread_mutex_t m_mutex;
     };
 
     /* 空锁(用于调试) */
